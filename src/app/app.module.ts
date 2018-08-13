ac9236424b9bb64527fdff71117a8a05ed640728
Apurv Shah <apurv278@outlook.com>
2018-08-13 11:44:35 -0400

    revision

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,38 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { AngularFireModule } from 'angularfire2'
+import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { HomePage } from '../pages/home/home';
+import { ChatPage } from '../pages/chat/chat';
+
+var config = {
+  apiKey: "AIzaSyCQpd1iuI-SpB-2NH0fM8d4j_fGZBh7lNc",
+  authDomain: "ionic-chat-app-f223e.firebaseapp.com",
+  databaseURL: "https://ionic-chat-app-f223e.firebaseio.com",
+  projectId: "ionic-chat-app-f223e",
+  storageBucket: "ionic-chat-app-f223e.appspot.com",
+  messagingSenderId: "72826839295"
+};
+
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(config),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    ChatPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
+import { ChatPage } from '../chat/chat';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+    username: string = '';
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
 
-  constructor(public navCtrl: NavController) {
-
+  }
+  alert(title: string, message: string){
+    let alert = this.alertCtrl.create({
+      title: 'Error',
+      subTitle: 'Invalid!!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+  loginUser(){
+    if(/^[a-zA-Z0-9]+$/.test(this.username)){
+        this.navCtrl.push(ChatPage, {
+          username: this.username
+        })
+    }else{
+      this.alert('Error', 'Invalid Usern!');
+    }
   }
 
 }
